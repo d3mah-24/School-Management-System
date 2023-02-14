cb784df839ea5d0ecc732143a5c5f7863eec71d6
Ahmed Mohammed <64283607+d3mah-24@users.noreply.github.com>
2023-02-14 18:57:05 +0300

    Update settings.py

--- sms/settings.py
+++ sms/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/design/'
 STATICFILES_DIRS = [
     BASE_DIR / "design",
